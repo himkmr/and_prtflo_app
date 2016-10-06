73d1c326fb424e7d7b2453c59b4eb114078bf49f
Himanshu <himkmr@gmail.com>
2016-10-05 21:55:27 -0500

    trying to pull

--- app/src/main/java/himkmr/and_prtflo_app/MainActivity.java
+++ app/src/main/java/himkmr/and_prtflo_app/MainActivity.java
@@ -2,6 +2,8 @@ package himkmr.and_prtflo_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //only method that deals with button clicks
+    public void onButtonClick(View view) {
+        Toast toast;
+        switch (view.getId()) {
+            case (R.id.button_bigger):
+                toast = Toast.makeText(getApplicationContext(), "This button will launch my Build It Bigger app", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+            case (R.id.button_cap):
+                toast = Toast.makeText(getApplicationContext(), "This will launch my Capstone app", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+            case (R.id.button_hawk):
+                toast = Toast.makeText(getApplicationContext(), "This will launch my Stock Hawk app", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+            case (R.id.button_material):
+                toast = Toast.makeText(getApplicationContext(), "This will launch my Material app", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+            case (R.id.button_movie):
+                toast = Toast.makeText(getApplicationContext(), "This will launch my Movies app", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+            case (R.id.button_ub):
+                toast = Toast.makeText(getApplicationContext(), "This will launch my Ubiquitous app", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+        }
+    }
+
+
 }
